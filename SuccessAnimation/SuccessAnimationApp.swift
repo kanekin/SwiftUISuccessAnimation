0b3f7e74be61379db84d13b2cc66a2aceb665ad5
Tatsuya Kaneko <tatsuya.j.kaneko@gmail.com>
2022-03-26 16:16:21 +0100

    Initial Commit

---
+++ SuccessAnimation/SuccessAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessAnimationApp.swift
+//  SuccessAnimation
+//
+//  Created by Tatsuya Kaneko on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuccessAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
